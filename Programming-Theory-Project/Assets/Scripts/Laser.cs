0894f7e067bf82c096c7b7645202b5280a5dbc18
andreasjonathanimm <jojonathanimmanuel@gmail.com>
2022-05-28 09:34:36 +0700

    LaserTypes
    - added a LaserType PowerUp
    - it SPINS! @

--- Programming-Theory-Project/Assets/Scripts/Laser.cs
+++ Programming-Theory-Project/Assets/Scripts/Laser.cs
@@ -7,4 +7,10 @@ public enum LaserType { Single, Tri, Spread, ABSSpread }
 public class Laser : MonoBehaviour
 {
     public LaserType laserType;
+    [SerializeField] private float rotationSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
 }
